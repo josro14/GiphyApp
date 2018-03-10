32ffb804e77f8ebc1e424bd8c5cca151e043fe78
JOSH-ROBERTS-PC\joshr <josro14@gmail.com>
2018-03-09 18:33:36 -0700

    coded gif display function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,28 @@
-var gifArr = [];
 
-$("button").on("click", function() {
+// Initial array of emotions
+var gifArr = ["happy", "sad", "disappointed", "excited"];
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=NTnDts5lUIHqZv3x6i94TkFIzfWczNEz";
+function displayGif() {
 
-    $.ajax({url:queryURL, 
-        method:"GET"})
-        .done(function(response){
-            console.log(response);
-        });
+    var theGIF = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theGIF + "&api_key=NTnDts5lUIHqZv3x6i94TkFIzfWczNEz";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+            //making div to hold the displayed gif
+            var gifDiv = $("<div class='theGIF'>");
+            var rating = results[i].rating;
+            var rateDisp = $("<p>").text("Rating: " + rating);
+            var gifImage = $("<img>");
+            gifImage.attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(rateDisp, gifImage);
+            $("#gif-spot").prepend(gifDiv);
+        }
     });
+
+}
