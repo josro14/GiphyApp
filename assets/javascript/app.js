6ff56ef19cd2885ba582f180943c0f6a2175a5b5
JOSH-ROBERTS-PC\joshr <josro14@gmail.com>
2018-03-11 21:53:59 -0600

    when buttons are clicked, their data-state changes from 'animate' to 'still', but I still can't get them to change from pause to play as they are clicked. Extra styling on buttons added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ var gifArr = ["happy", "sad", "disappointed", "excited"];
 function displayGif() {
 
     var theGIF = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theGIF + "&api_key=NTnDts5lUIHqZv3x6i94TkFIzfWczNEz";
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + theGIF + "&api_key=NTnDts5lUIHqZv3x6i94TkFIzfWczNEz&limit=10";
 
     $.ajax({
         url: queryURL,
@@ -14,13 +14,28 @@ function displayGif() {
 
         var results = response.data;
         for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div class='theGIF'>");
+            var gifDiv = $("<div id='theGIF' data-state='animated' data-url-still='results[i].images.fixed_height_still.url' data-url-animated='results[i].images.fixed_height.url'>");
             var rating = results[i].rating;
             var rateDisp = $("<p>").text("Rating: " + rating);
             var gifImage = $("<img>");
             gifImage.attr("src", results[i].images.fixed_height.url);
+            //gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+            //gifImage.attr("data-animate", results[i].images.fixed_height.url);
             gifDiv.append(rateDisp, gifImage);
             $("#gif-spot").prepend(gifDiv);
+
+            $("#theGIF").on("click", function () {
+                var state = $(this).attr("data-state");
+                console.log(state);
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                }
+                else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                }
+            })
         }
     });
 }
@@ -36,7 +51,10 @@ function renderButtons() {
     }
 }
 
+
+
 $("#add-gif").on("click", function (event) {
+   // $("#gif-input").val(""); this is supposed to clear the value in the input box, but it clears the button value as well...
     event.preventDefault();
     var theGIF = $("#gif-input").val().trim();
     gifArr.push(theGIF);
